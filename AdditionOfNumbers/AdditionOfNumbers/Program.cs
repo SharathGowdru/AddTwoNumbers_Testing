6ee47475357c1946d57eb91d22371e4235218647
Sharath <Sharus9587@gmail.com>
2021-05-20 19:38:51 +0530

    Added : Addition of two numbers

---
+++ AdditionOfNumbers/AdditionOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOfNumbers
+{
+               public class Program
+    {         
+            public static void Main()
+            {
+
+            }
+            public int Add(int numOne, int numTwo)
+            {
+                int sum = numOne + numTwo;
+                return sum;
+            }
+        }
+    }
+   
---
+++ AdditionOfNumbers/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed87f3a3-76d0-4270-aea8-a182d66b00ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
